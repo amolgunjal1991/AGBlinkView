ecb727c7faf683cd09f7c3f5e64a14570c96ff96
Amol Gunjal <amolgunjal61@yahoo.com>
2021-08-12 11:29:28 +0530

    Add BlinkView

---
+++ AGBlinkView/Classes/AGBlinkView.swift
@@ -0,0 +1,38 @@
+
+
+import UIKit
+
+
+
+public class AGBlinkView : UIView {
+    
+    //MARK:- Variables
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    var colorCounter = 0
+    
+    //MARK:- Lifecycle methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        scheduleColorChange()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK:- Change the backgroundcolor with timee after some interval of time to blink.
+    func scheduleColorChange() {
+        if #available(iOS 10.0, *) {
+            let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0,
+                                                             repeats: true) { (timer) in  //1
+                UIView.animate(withDuration: 2.0) {  //2
+                    self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor  //3
+                    self.colorCounter += 1  //4
+                }
+            }
+            scheduledColorChanged.fire()  //5
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
--- Example/AGBlinkView/ViewController.swift
+++ Example/AGBlinkView/ViewController.swift
@@ -7,18 +7,35 @@
 //
 
 import UIKit
+import AGBlinkView
 
 class ViewController: UIViewController {
 
+    //MARK:- Variables
+    var blinkView = AGBlinkView()
+    
+    //MARK:- Liecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupBlinkView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK:- Setup the blinkview
+    func setupBlinkView() {
+        self.view.addSubview(blinkView)
+        blinkView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            blinkView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            blinkView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            blinkView.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height),
+            blinkView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width)
+        ])
+    }
+    
 }
 
